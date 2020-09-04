60c51bbfdb22cc141c4ff29423e5502ecdc1b1eb
XUEHAISHAN <xuehaishan@kingsoft.com>
2020-09-04 14:31:02 +0800

    remove log

--- pkg/kube/kube.go
+++ pkg/kube/kube.go
@@ -1,7 +1,6 @@
 package kube
 
 import (
-	"fmt"
 	"os"
 
 	"k8s.io/client-go/kubernetes"
@@ -10,25 +9,9 @@ import (
 )
 
 func Config() (*restclient.Config, error) {
-	// config, err := restclient.InClusterConfig()
-	// if err == nil {
-	// 	return config, nil
-	// }
-
 	kubeconfigPath := os.Getenv("KUBE_CONFIG")
-	fmt.Println("-------kubeconfigPath", kubeconfigPath)
 	return clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 }
-
-// func ConfigForCluster(clusterID string) (*restclient.Config, error) {
-// 	config, err := restclient.InClusterConfig()
-// 	if err == nil {
-// 		return config, nil
-// 	}
-
-// 	kubeconfigPath := os.Getenv("KUBE_CONFIG")
-// 	return clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-// }
 
 func Client() (*kubernetes.Clientset, error) {
 	config, err := Config()
